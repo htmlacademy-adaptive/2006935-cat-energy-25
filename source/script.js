bf97135ebf222db6218f2b23244f676223872a5b
Stuloviv <stuloviv@gmail.com>
2022-05-06 21:49:23 +0400

    Вносит правки: декоративные элементы сделаны псевдоэлементами, константы в JS

--- source/script.js
+++ source/script.js
@@ -1,5 +1,5 @@
-let navMain = document.querySelector(".main-nav");
-let navToggle = document.querySelector(".main-nav__toggle");
+const navMain = document.querySelector(".main-nav");
+const navToggle = document.querySelector(".main-nav__toggle");
 
 navMain.classList.remove("main-nav--nojs");
 
@@ -12,3 +12,20 @@ navToggle.addEventListener("click", function () {
     navMain.classList.remove("main-nav--opened");
   }
 });
+
+// const before = document.querySelector(".slider__before-image");
+// const after = document.querySelector(".slider__after-image");
+// const switcher = document.querySelector(".slider__scale-switcher");
+
+
+// // navMain.classList.remove("main-nav--nojs");
+
+// switcher.addEventListener("click", function () {
+//   if (before.classList.contains("slider__before-image")) {
+//     navMain.classList.remove("main-nav--closed");
+//     navMain.classList.add("main-nav--opened");
+//   } else {
+//     navMain.classList.add("main-nav--closed");
+//     navMain.classList.remove("main-nav--opened");
+//   }
+// });
